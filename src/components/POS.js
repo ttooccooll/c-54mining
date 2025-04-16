9f0fcfe93cb96dabb65ad63b747f29ed612da62f
Jason <jasonbohio@gmail.com>
2025-04-16 12:54:15 -0400

    shops up

--- src/components/POS.js
+++ src/components/POS.js
@@ -3,7 +3,7 @@ import React from 'react';
 const POS = () => {
   return (
     <iframe 
-      src='https://heyheyyo.duckdns.org/apps/rPds9rv94zjjavnw5PVTYhbbpcG/pos' 
+      src='http://heyheyyo.duckdns.org:3003/apps/rPds9rv94zjjavnw5PVTYhbbpcG/pos' 
       style={{ maxWidth: '100%', border: 0, height: '90vh', width: '90%', borderRadius: '20px' }} 
       title="POS System"
     ></iframe>
