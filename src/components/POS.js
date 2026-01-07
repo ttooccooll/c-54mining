1696a3582c3529772278fc0695f5b2a0b7f47726
Jason <jasonbohio@gmail.com>
2026-01-07 11:51:55 -0500

    pos

--- src/components/POS.js
+++ src/components/POS.js
@@ -3,7 +3,7 @@ import React from 'react';
 const POS = () => {
   return (
     <iframe 
-      src='https://btcpay.bitcoinisforcriminals.com/apps/rPds9rv94zjjavnw5PVTYhbbpcG/pos' 
+      src='https://checkout.square.site/merchant/ML2CN2CP8J201/checkout/FXUW57O7IMIQ4S3PETHDW4PL' 
       style={{ maxWidth: '100%', border: 0, height: '90vh', width: '90%', borderRadius: '20px' }} 
       title="POS System"
     ></iframe>
