d1f2b663d52993a999ad773097695d6ab37411dd
ttooccooll <jasonbohio@gmail.com>
2026-06-11 12:07:15 -0400

    Fix typo: loved family -> loved ones

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -19,7 +19,7 @@ const Home = () => {
       </Helmet>
       <div style={{ padding: '20px' }} className='quote' ref={quoteRef}>
       <h1 className='home'>Heating, Mining, and Home Automation</h1>
-      <p className='homep'>Inspired by the Berlin Airlift, we aim to provide the tools for you to be a part of the most powerful movement of our generation. All at once, you will be enabling financial inclusion to people worldwide while providing privacy and energy independence for your loved family.</p>
+      <p className='homep'>Inspired by the Berlin Airlift, we aim to provide the tools for you to be a part of the most powerful movement of our generation. All at once, you will be enabling financial inclusion to people worldwide while providing privacy and energy independence for your loved ones.</p>
       <div id='q'>
         <p className='homep'>“This technology goes around blockades and government restrictions, it allows you to move value without trusting anyone, it connects you to the world, and it allows you to empower yourself and do things that are otherwise impossible.”</p>
         <p className='homep'>― Alex Gladstein, Check Your Financial Privilege</p>
