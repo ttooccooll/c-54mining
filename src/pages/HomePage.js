8e0432148d15cb5b500718b60a6d9b55d408be77
ttooccooll <jasonbohio@gmail.com>
2026-06-11 11:52:36 -0400

    home quote

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -13,7 +13,7 @@ const Home = () => {
   return (
     <div style={{ padding: '20px' }} className='quote' ref={quoteRef}>
       <h1 className='home'>Heating, Mining, and Home Automation</h1>
-      <p className='homep'>Inspired by the Berlin Airlift, we aim to provide the tools for you to be a part of the most powerful movement of our generation, enabling financial inclusion to people worldwide while enabling privacy and energy independence for yourself and your loved ones.</p>
+      <p className='homep'>Inspired by the Berlin Airlift, we aim to provide the tools for you to be a part of the most powerful movement of our generation. All at once, you will be enabling financial inclusion to people worldwide while providing privacy and energy independence for your loved family.</p>
       <div id='q'>
         <p className='homep'>“This technology goes around blockades and government restrictions, it allows you to move value without trusting anyone, it connects you to the world, and it allows you to empower yourself and do things that are otherwise impossible.”</p>
         <p className='homep'>― Alex Gladstein, Check Your Financial Privilege</p>
